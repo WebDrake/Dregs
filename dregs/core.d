1825e8984ac6d63bb7d02491b87d716a6278e3e8
Joseph Rushton Wakeling <joe@webdrake.net>
2012-04-27 16:45:55 +0200

    Remove private variables from Rating struct.

--- dregs/core.d
+++ dregs/core.d
@@ -2,29 +2,14 @@ module dregs.core;
 
 struct Rating(UserID = size_t, ObjectID = size_t, Reputation = double)
 {
-	private UserID user_;
-	private ObjectID object_;
-	private Reputation weight_;
+	UserID user;
+	ObjectID object;
+	Reputation weight;
 
 	this(UserID u, ObjectID o, Reputation r)
 	{
-		user_ = u;
-		object_ = o;
-		weight_ = r;
-	}
-
-	final pure nothrow UserID user()
-	{
-		return user_;
-	}
-
-	final pure nothrow ObjectID object()
-	{
-		return object_;
-	}
-	
-	final pure nothrow Reputation weight()
-	{
-		return weight_;
+		user = u;
+		object = o;
+		weight = r;
 	}
 }
